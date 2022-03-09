614b5eb9a4967b95e2688c9284ee27491a2d1a7b
Elian64 <edeniz@iesrafaelalberti.es>
2022-03-09 21:41:41 +0100

    avance de proyecto

--- src/main/kotlin/proyectoClase.kt
+++ src/main/kotlin/proyectoClase.kt
@@ -15,71 +15,83 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.skija.Surface
+import javax.swing.text.Style
 
 @Composable
 fun Combate() {
-    var text by remember { mutableStateOf("Hello, World!") }
 
     DesktopMaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-
-}
-
-@Composable
-
-fun cuadrado1(color1: Color, color2: Color) {
-    Surface(
-        color = color1,
-        modifier = Modifier.size(60.dp)
-    ) {}
-    Surface(
-        color = color2,
-        modifier = Modifier.size(60.dp)
-    ) {}
-}
-
-@Composable
-fun cuadrado2(color1: Color, color2: Color) {
-    Surface(
-        color = color1,
-        modifier = Modifier.size(60.dp)
-    ) {}
-    Surface(
-        color = color2,
-        modifier = Modifier.size(60.dp)
-    ) {}
-
-}
-
-@Composable
-fun aplicacionUsoRowCOlumn() {
-    Surface(
-        color = Color.Black, //fondo
-        modifier = Modifier.fillMaxSize() //lo ajustamos para que ocupe el maximo de la ventana
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+        Surface(
+            color = Color.LightGray,
+            modifier = Modifier.fillMaxSize()
         ) {
-            cuadrado1(Color.Magenta, Color.Green)
-        }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            cuadrado2(Color.Yellow, Color.White)
+
+            Row( //Row de movimientos
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                movimientos()
+            }
+
+            Row( //Row de vida
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.End
+            ) {
+                cuadradoVida(9)
+            }
         }
     }
 }
 
+
+@Composable
+fun cuadradoVida(numeroCuadrados: Int) {
+    for (i in 0..numeroCuadrados) {
+        Surface(
+            color = Color.Green,
+            modifier = Modifier.size(30.dp)
+        ) {}
+    }
+}
+
+@Composable
+fun movimientos() {
+
+    //var text by remember { mutableStateOf("M1") }
+
+    val mov1 = remember { mutableStateOf("M1") }
+    val mov2 = remember { mutableStateOf("M2") }
+    val mov3 = remember { mutableStateOf("M3") }
+    val mov4 = remember { mutableStateOf("M4") }
+
+    val lista = listOf(mov1, mov2, mov3, mov4)
+
+    for (movimiento in lista) {
+        Surface(
+            color = Color.White,
+            modifier = Modifier.size(50.dp)
+        ) {
+            /*Button(onClick = {
+                cuadradoVida(7)
+            }) {
+                Text(text)
+            }*/
+            Text(text = movimiento.value,
+                modifier = Modifier.clickable
+                {
+                    //movimiento.value = "OK"
+                    cuadradoVida(7) // Aqui debe restar cuadrados de vida
+
+                })
+
+        }
+
+    }
+}
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        aplicacionUsoRowCOlumn()
+        Combate()
     }
 }
